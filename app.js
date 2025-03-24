0d3f70ca53fc3e10ca58af1f5bfcae351e8bc0f0
VarathanN <varathanseeyou@gmail.com>
2025-03-24 17:49:38 +0530

    5th commit

--- app.js
+++ app.js
@@ -8,8 +8,22 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
 app.get("/", (req, res) => {
-  res.render("index", { message: "ejs rocks!" });
+  res.render("index", { links: links, users: users });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { links: links });
 });
 
 app.get("/error-next", (req, res, next) => {
